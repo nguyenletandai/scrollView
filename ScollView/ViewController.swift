f4bbc1da6f4c02f0ecd549f9b858ba030407889e
nguyenletandai <nguyenletandai@gmail.com>
2018-12-23 09:11:42 +0700

    Up code

---
+++ ScollView/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  ScollView
+//
+//  Created by daicudu on 11/19/18.
+//  Copyright © 2018 daicudu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
